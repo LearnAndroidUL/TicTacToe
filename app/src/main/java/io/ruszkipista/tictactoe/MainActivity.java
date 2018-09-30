9fa1eff84075a31c7121249ba8b6997396e5630e
Istvan Orosz <16229509@studentmail.ul.ie>
2018-09-30 11:23:40 +0100

    built view - board

--- app/src/main/java/io/ruszkipista/tictactoe/MainActivity.java
+++ app/src/main/java/io/ruszkipista/tictactoe/MainActivity.java
@@ -1,7 +1,9 @@
 package io.ruszkipista.tictactoe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mGame = new TicTacToeGame(this);
+    }
+
+    public void pressedBoardButton(View view){
+
+        Toast.makeText(this, "Pressed "+view.getTag(), Toast.LENGTH_SHORT).show();
+    }
+    public void pressedNewGame(View view){
+        Toast.makeText(this, "Pressed NewGame", Toast.LENGTH_SHORT).show();
     }
 }
